c22bf7b7bf476f3216bc2d6f73bdc2157bcf470e
Nadeen Gamage <nadeengamage@gmail.com>
2022-03-30 13:52:01 +0530

    FIxed created by user issue.

---
+++ src/pages/ApplicationManagement/ReportDetails/Document/index.js
@@ -0,0 +1,102 @@
+import PropTypes from "prop-types";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import MetaTags from "react-meta-tags";
+import {
+  Row,
+  Col,
+  CardTitle,
+  Card,
+  CardBody,
+  Container,
+  Button
+} from "reactstrap";
+
+import Loader from "components/SyncLoader";
+
+import FileViewer from 'react-file-viewer';
+
+//APIs
+import {
+  getProNoteReport
+} from "services/report.service";
+
+const ReportDocument = (props) => {
+
+  const { reportType } = useParams();
+  const { appraisalId } = useParams();
+
+  const [type, setType] = useState(null);
+  const [data, setData] = useState(null);
+
+  const onError = (error) => {
+    console.error(error, 'error in file-viewer');
+  }
+
+  const download = () => {
+    window.open(data);
+  }
+
+  useEffect(async () => {
+    var _isMounted = true;
+    if (reportType === 'pro-note') {
+      const response = await getProNoteReport(appraisalId);
+
+      if (response != undefined) {
+    
+        var fileURL = URL.createObjectURL(new Blob([response.data], { type: 'application/pdf' }));
+
+        if (_isMounted) {
+          setData(fileURL);
+          setType('pdf');
+        }
+      }
+
+      return () => {
+        _isMounted = false;
+      };
+    }
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div className="page-content">
+        <MetaTags>
+          <title>Report | Pakoman Digital Loan | Mobile Solutions - LOITS</title>
+        </MetaTags>
+
+        <Container fluid>
+          <Row>
+            <Col lg={12}>
+              <Card>
+                <CardBody>
+                  <Loader loading={true}><p>test details</p></Loader>
+                  <CardTitle className="h4"></CardTitle>
+                  <div className="d-flex justify-content-between">
+                  <p className="card-title-desc"> Reports.</p>
+                  <Button size="md" color="success" onClick={download}><i className="fa fa-download me-2"></i>Download</Button>
+                  </div>
+
+                  <div key={1} className="d-flex flex-column document-wrapper">
+                    <div className="justify-content-center">
+                    {data != null && <FileViewer
+                      fileType={type}
+                      filePath={data}
+                      onError={onError} />}
+                    </div>
+                  </div>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </React.Fragment>
+  );
+}
+
+ReportDocument.propTypes = {
+  match: PropTypes.any
+};
+
+export default ReportDocument;
--- src/pages/ApplicationManagement/index.js
+++ src/pages/ApplicationManagement/index.js
@@ -26,6 +26,7 @@ import IncomeExpensesDetails from "./IncomeExpensesDetails";
 import LiabilityDetails from "./LiabilityDetails";
 import CreditScoringDetails from "./CreditScoringDetails";
 import ImageDetails from "./ImageDetails";
+import ReportDetails from "./ReportDetails";
 import ApprovalDetails from "./ApprovalDetails";
 
 const Appraisal = () => {
@@ -89,6 +90,10 @@ const Appraisal = () => {
 
                   <AccordionBody title="IMAGE DETAILS">
                     <ImageDetails active={"9"} />
+                  </AccordionBody>
+
+                  <AccordionBody title="REPORT DETAILS">
+                    <ReportDetails active={"11"} />
                   </AccordionBody>
 
                   <AccordionBody title="APPROVAL DETAILS">
