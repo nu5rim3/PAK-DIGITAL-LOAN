02a56f019bb096636d90bef86491ec3c2f38ec5c
Nadeen Gamage <nadeengamage@gmail.com>
2022-05-30 18:29:22 +0530

    customer undertaking secion added.

--- src/pages/ApplicationManagement/CustomerDetails/undertaking.js
+++ src/pages/ApplicationManagement/CustomerDetails/undertaking.js
@@ -9,8 +9,6 @@ import {
 
 import Grid from '@material-ui/core/Grid'
 
-import NumberFormat from "react-number-format";
-
 import Loader from "components/Loader";
 import AsyncImage from "pages/ApplicationManagement/ImageDetails/async_images";
 
@@ -29,7 +27,7 @@ const UndertakingDetails = (props) => {
 
   const getCustomerAknowledgement = () => {
     return <p style={{ direction: 'rtl' }}>
-      میں بقائمی ہوش وحواس <u>{""}</u> کو دئیے جانے والے مبلغ <u><NumberFormat value={"10000"} displayType={'text'} thousandSeparator={true} /></u> روپے کے قرض کی ضمانت قبول کرتا ہوں اور بلا مشروط اقرار کرتا ہوں کہ اگر قرض خواہ لیے گئے قرض کو کسی بھی وجہ سے ادا کرنے سے قاصر رہتا ہے تو اس قرض کی مکمل ادائیگی کی ذمہ داری مجھ پر ہوگی اور میں اُوپر دی گئی ذاتی اور قرض خواہ کے بارے میں دی گئی معلومات کے بارے میں پوری طرح متفق ہوں.
+      میں ا قرار کر تا/کر تی ھوں کہ اوپر دی گئی تمام معلومات میری علم کے مطابق درست ہیں اور کوئی آمر پوشیدہ نہیں رکھا گیا اور دی گئی معلومات اگر غلط ثابت ہوں تو پاک عمان مائکرو فنانس بینک میری قرضے کی درخواست کو مسترد کر سکتا ہے ۔مزید یہ کہہ میں پاک عمان بینک کو یہ حق بھی دیتا/دیتی ہوں کہ وہ میری معلومات کا تبادلہ کسی بھی بینک/کریڈٹ بیورو/فنانشل انسٹیٹیوٹ یا کمپنی کے ساتھ کر سکتا ہے اور یہ حق بھی رکھتا ہے کہ ان معلومات کا تبادلہ اسٹیٹ بینک کے ECIB کے ساتھ بھی کر سکتا ہے
     </p>;
   }
 
@@ -60,7 +58,7 @@ const UndertakingDetails = (props) => {
             <Row>
               <div className="text-muted d-flex">
                 <Grid item xs={12}>
-                  <Card className="witness-signature-card">
+                  <Card className="customer-signature-card">
                     <p style={{ textAlign: 'right' }}>
                       {getCustomerAknowledgement()}
                     </p>
@@ -71,7 +69,7 @@ const UndertakingDetails = (props) => {
             <Row>
               <div className="text-muted d-flex">
                 <Grid item xs={6}>
-                  <Card className="witness-signature-card">
+                  <Card className="customer-signature-card">
                     <p>Customer Thumbnail</p>
                     {thumb && thumb.length > 0 && thumb.map((sign, index) => (
                       sign.status === "A" && <AsyncImage src={sign.imgPath} key={index} />
@@ -79,7 +77,7 @@ const UndertakingDetails = (props) => {
                   </Card>
                 </Grid>
                 <Grid item xs={6}>
-                  <Card className="witness-signature-card">
+                  <Card className="customer-signature-card">
                     <p>Customer Signature</p>
                     {signature && signature.length > 0 && signature.map((sign, index) => (
                       sign.status === "A" && <AsyncImage src={sign.imgPath} key={index} />
