3be1e759da5c7a8f8eb5e24463f8de497f4830cd
Nadeen Gamage <nadeengamage@gmail.com>
2022-06-06 01:03:30 +0530

    Fixed issue was the pro note report url.

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -1,7 +1,7 @@
 import { get, blob } from "helpers/api_helper";
 
 export const getProNoteReport = (appraisalId) => {
-    return blob(`/mobixCamsLoan/v1/loans/resources/pro-note-report/${appraisalId}`);
+    return blob(`/mobixCamsReport/v1/reports/pro-note/${appraisalId}/report.pdf`);
 };
 
 export const getIqarNamaReport = (appraisalId) => {
