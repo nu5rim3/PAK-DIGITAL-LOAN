2ca0f834bb61eab0f9509485ea7bdf3b9834fa40
Sachindu Peramuna <sachinduayodye@gmail.com>
2022-09-30 16:29:24 +0530

    added view pdf feature to image section

--- src/pages/ApplicationManagement/ImageDetails/async_images.js
+++ src/pages/ApplicationManagement/ImageDetails/async_images.js
@@ -1,5 +1,13 @@
 import PropTypes from "prop-types";
 import React, { useEffect, useState } from "react";
+import { FileViewer } from "react-file-viewer";
+import {
+  Row,
+  Col,  
+  Card,
+  CardBody,
+  Container
+} from "reactstrap";
 
 //Lightbox
 import Lightbox from "react-image-lightbox-rotate-fixed";
@@ -14,17 +22,37 @@ const AsyncImage = (props) => {
 
     const [data, setdata] = useState(null);
     const [isFits, setisFits] = useState(false);
+    const [type, setType] = useState(null);
 
     useEffect(async () => {
         var _isMounted = true;
         if (props.src) {
-            const response = await viewImage(props.src);
-
+            const response = await viewImage(props.src); 
+            // console.log(response);
+            
             var data = null;
-            if (response != undefined && response != null) {
-                data = `data:${response?.headers['content-type']};base64,${new Buffer(response?.data).toString('base64')}`;
-            } 
-            _isMounted && setdata(data);
+            var contentType = null;
+            if (response != undefined && response != null) {  
+                
+                contentType = `data:${response?.headers['content-type']}`;
+                //console.log(contentType);
+
+                if (contentType == "application/pdf") {
+                    data = `${contentType};base64,${new Buffer(response?.data).toString('base64')}`;
+
+                    var fileURL = URL.createObjectURL(data);
+                    window.open(fileURL);
+
+                    if (_isMounted) {
+                        setdata(fileURL);
+                        setType('pdf');
+                    }
+                } else {                
+                    data = `${contentType};base64,${new Buffer(response?.data).toString('base64')}`;
+                    _isMounted && setdata(data);
+                }
+            }            
+            
 
             return () => {
                 _isMounted = false;
@@ -32,8 +60,28 @@ const AsyncImage = (props) => {
         }
     }, [data]);
 
-    if (data) {
-        return (
+
+    if (data && type == 'pdf') {
+        return (                           
+            <Container fluid>
+                <Row>
+                    <Col lg={12}>
+                    <Card>
+                        <CardBody>
+                            <div key={1} className="document-wrapper">
+                                { <FileViewer
+                                fileType={type}
+                                filePath={data}
+                                />}
+                            </div>
+                        </CardBody>
+                    </Card>
+                    </Col>
+                </Row>
+            </Container>          
+        )
+    } else {
+        return (                           
             <div className={props.className}>
                 <img src={data} onClick={() => setisFits(true)} className="img-responsive" style={{
                     width: "200px",
@@ -53,8 +101,8 @@ const AsyncImage = (props) => {
                         }}
                     />
                 ) : null}
-            </div>
-        );
+            </div>              
+        )
     }
 
     return null;
